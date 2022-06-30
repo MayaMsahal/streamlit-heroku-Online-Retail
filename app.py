ab835e6d901ae541d0be504eb50fb33b9924c19f
MayaMsahal <108496822+MayaMsahal@users.noreply.github.com>
2022-06-30 23:57:03 +0100

    code
    code

--- app.py
+++ app.py
@@ -1,20 +1,46 @@
 import streamlit as st
+import pandas as pd
+import pickle
 
-def main():
+st.image("http://www.ehtp.ac.ma/images/lo.png")
+st.write("""
+# MSDE4 
+##### Etudiantes:Msahal Maya et ZERROUKI Asmae
+## Project:Customer segmentation using RFM analysis and clustering
+This app predicts the **Income of online-retail using K-mean Cluster and RFM ** type
+""")
+st.sidebar.image("pic.jpeg",width=300)
 
-    # Defining header title
-    st.sidebar.title("Streamlit Forecasting")
-    st.sidebar.title("What to do")
-    app_mode = st.sidebar.selectbox(
-        "Choose the app mode", ["Show instructions", "Run Forecasting"]
-    )
+st.sidebar.header('User Input Parameters')
 
-    # Defining control flow of the app
-    if app_mode == "Show instructions":
-        st.sidebar.success('To continue select "Run Forecasting"')
-    elif app_mode == "Run Forecasting":
-        st.text("Forecasting")
+def user_input_features():
+    online_retail_Recency = st.sidebar.slider('Recency', 1.0, 75.0)
+    online_retail_Frequency = st.sidebar.slider('Frequency', 1.0, 10.0)
+    online_retail_length = st.sidebar.slider('MonetaryValue', 1.0, 160.8)
+    online_cluster = st.sidebar.slider('Cluster', 1, 160)
+    
+    data = {'Recency': online_retail_Recency,
+            'Frequency': online_retail_Frequency,
+            'MonetaryValue': online_retail_length,
+            'Cluster': online_cluster}
+			
+			
+    features = pd.DataFrame(data,index=[0])
+    return features
+
+df = user_input_features()
+st.subheader('User Input parameters')
+st.write(df)
+
+#K-Means model:
+
+model_km=pickle.load(open("model_df_normalized.pkl", "rb"))
+print(model_km)
+prediction = model_km.predict(df)
 
 
-if __name__ == "__main__":
-    main()
+st.subheader('Prediction')
+st.write(prediction)
+
+
+
